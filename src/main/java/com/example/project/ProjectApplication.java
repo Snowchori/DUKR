9c69f0963d0e46a77f51626e6378cefdfe89681f
Snowchori <snowchori@naver.com>
2023-08-03 21:58:52 +0900

    setting war

--- src/main/java/com/example/project/ProjectApplication.java
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -3,12 +3,19 @@ package com.example.project;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.example.controller", "com.example.model", "com.example.project"})
 @MapperScan( basePackages = { "com.example.config" } )
-public class ProjectApplication {
+public class ProjectApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ProjectApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
